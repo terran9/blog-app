0cc65606fc46cd4f38c71b09d8dd396b71c93e08
Venkata Nagendra Tatakunta <nagendra.tatakuntla@gmail.com>
2024-06-03 00:52:13 +0530

    Adding custom exception handler

--- src/main/java/com/terrancode/blog/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/terrancode/blog/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,20 @@
 package com.terrancode.blog.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.terrancode.blog.dto.BlogAppResponse;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<BlogAppResponse> resourceNotFoundExceptionHandler(ResourceNotFoundException e){
+		String message = e.getMessage();
+		BlogAppResponse apiResponse = new BlogAppResponse(message, false);
+		return new ResponseEntity<BlogAppResponse>(apiResponse, HttpStatus.NOT_FOUND);
+	}
 
 }
