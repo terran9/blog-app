7e29ddc31162fe50301f67bddf5297a71c2241e6
Venkata Nagendra Tatakunta <nagendra.tatakuntla@gmail.com>
2024-07-21 21:22:12 +0530

    handleMethodArgumentNotValidException

--- src/main/java/com/terrancode/blog/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/terrancode/blog/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,12 @@
 package com.terrancode.blog.exceptions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -15,6 +20,18 @@ public class GlobalExceptionHandler {
 		String message = e.getMessage();
 		BlogAppResponse apiResponse = new BlogAppResponse(message, false);
 		return new ResponseEntity<BlogAppResponse>(apiResponse, HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+		Map<String, String> response = new HashMap<>();
+		e.getBindingResult().getAllErrors().forEach(error -> {
+			String fieldName = ((FieldError) error).getField();
+			String defaultMessage =  error.getDefaultMessage();
+			response.put(fieldName,defaultMessage);
+		} );
+		
+		return new ResponseEntity<Map<String, String>>(response, HttpStatus.BAD_REQUEST);
 	}
 
 }
